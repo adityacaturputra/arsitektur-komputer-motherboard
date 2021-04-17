c0091dea22ed23ddb509d7e7734832d2d5874412
adityacaturputra <adityacaturputra25@gmail.com>
2021-04-17 17:21:27 +0700

    update modul komponen linknya belum

---
+++ materi/resources/komponen.js
@@ -0,0 +1,144 @@
+const komponens = [
+    {
+        name    : "Slot Processor",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/Socket-Processor-300x229.jpg",
+        artikel : "Slot Processor adalah tempat peletakan prosesor pada motherboard. Dudukan ini berbentuk segi empat dengan lubang-lubang kecil tempat tertancapnya kaki-kaki (pin-pin) prosesor yang tersusun membentuk matriks 2 dimensi. Yang berfungsi untuk meletakan processor ke motherboard.",
+        link    : "https://www.youtube.com/watch?v=SZBSGEYhjnM"
+    },
+    {
+        name    : "Konektor power",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/ATX-Power-300x270.jpg",
+        artikel : "konektor power pada motherboard adalah Pin yang menyambungkan motherboard dengan power supply di casing sebuah komputer. Agar si motherboard menerima daya dari power supply.",
+        link    : ""
+    },
+    {
+        name    : "Northbridge",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/Northbridge.jpg",
+        artikel : "Northbridge adalah komponen yang menghubungkan procossor (CPU) ke sistem memori dan graphics controller (AGP dan PCI Express) melalui bus berkecepatan tinggi, dan ke southbridge. Fungsi dari Northbridge sendiri yaitu menjembatani arus data di sekitar main Memory, Prosesor, Front Side Bus dan AGP Bus juga mengatur kerja power management.",
+        link    : ""
+    },
+    {
+        name    : "4.	Slot Memory RAM",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/Slot-memory-300x200.jpg",
+        artikel : "Slot Memory RAM berfungsi untuk memasang memory card (RAM), Memory atau RAM ada beberapa Jenis seperti SDRAM,DDR1,DDR2,DDR3 dan DDR4.",
+        link    : ""
+    },
+    {
+        name    : "5.	Port SATA",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/Port-Sata-300x212.jpg",
+        artikel : "SATA singkatan dari Serial ATA yang berfungsi untuk menghubungkan Media penyimpanan seperti hardisk atau SSD ke Motherboard.",
+        link    : ""
+    },
+    {
+        name    : "6.	Slot PCI-E",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/Slot-PCIE-300x163.jpg",
+        artikel : "PCI Express (PCI–E/PCIex) adalah slot ekspansi yang didesain untuk menggantikan PCI bus yang lama. Banyak Motherboard mengadopsi PCI express karena PCI Express memiliki transfer data yang lebih cepat, terutama untuk keperluan grafis 3D.",
+        link    : ""
+    },
+    {
+        name    : "7.	Southbridge",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/southbridge.jpg",
+        artikel : "adalah chip dalam arsitektur chipset logika inti pada bagian utara motherboard PC. Fungsi dari Southbridge adalah mengatur kerja perangkat semacam IDE Controller, PCI Bus, ROM Bios, Keyboard & Mouse, USB, Eth. LAN, Modem dan fungsi I/O lainnya.",
+        link    : ""
+    },
+    {
+        name    : "8.	LED",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/Lampu-LED-300x194.jpg",
+        artikel : "Power LED adalah lampu menyala jika terdapat standby power di Motherboard. LED ini bertindak sebagai reminder (pengingat) untuk mematikan system power sebelum menghidupkan atau mematikan komputer.",
+        link    : ""
+    },
+    {
+        name    : "9.	Port PS/2",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/ps2_ports_atx-174x300.jpg",
+        artikel : "Port PS/2 adalah konektor mini-DIN-6pin yang menghubungkan mouse ke komputer.",
+        link    : ""
+    },
+    {
+        name    : "10.	Port Paralel",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/Port-Paralel-300x225.jpg",
+        artikel : "Fungsi dari port paralel adalah menyambungkan komputer dengan printer, scanner, sampai dengan menghubungkan komputer dengan perangkat tambahan tertentu yang dirancang menggunakan koneksi port paralel.",
+        link    : ""
+    },
+    {
+        name    : "11.	Port Serial",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/serial-300x154.jpg",
+        artikel : "Port Serial adalah Port asinkron yang ada pada komputer yang digunakan untuk menghubungkan perangkat serial ke komputer dan mampu mengirimkan sedikit demi sedikit.",
+        link    : ""
+    },
+    {
+        name    : "12.	Port VGA",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/vga-300x225.jpg",
+        artikel : "Port VGA adalah sebuah antarmuka analog pada kartu grafis yang umumnya digunakan untuk menghubungkan antara komputer dengan monitor.",
+        link    : ""
+    },
+    {
+        name    : "13.	Port DVI",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/dvi-300x120.jpg",
+        artikel : "Port DVI adalah interface video display yang dikembangkan oleh Digital Display Working Group (DDWG). Interface digital ini digunakan untuk menghubungkan sumber video, seperti kartu grafis ke perangkat layar, seperti monitor komputer.",
+        link    : ""
+    },
+    {
+        name    : "14.	Port HDMI",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/hdmi.jpg",
+        artikel : "Port HDMI adalah interface yang berfungsi untuk menghubungkan Display ke komputer, seperti monitor, TV, Proyektor, dll.",
+        link    : ""
+    },
+    {
+        name    : "15.	Port USB",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/usb.jpg",
+        artikel : "Port USB berfungsi untuk menghubungkan perangkat seperti mouse,keyboard, flashdisk, printer, dan perangkat lainnya.",
+        link    : ""
+    },
+    {
+        name    : "16.	Battery CMOS",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/cmos-300x252.png",
+        artikel : "sebuah baterai yang digunakan oleh BIOS untuk tetap aktif meski tanpa aliran listrik.",
+        link    : ""
+    },
+    {
+        name    : "17.	Port RJ-45",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/rj45-300x203.png",
+        artikel : "Port RJ-45 berfungsi untuk menghubungkan atau mengkoneksikan Internet pada komputer.",
+        link    : ""
+    },
+    {
+        name    : "18.	Port Audio",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/audio.jpg",
+        artikel : `Pada Port Audio: <br>
+        -	Port Line in jack(biru): untuk menghubungkan sumber dari audio. <br>
+        -	Port Line out jack(hijau): untuk menghubungkan ke headphone atau headset. <br>
+        -   Port Mikrofon (pink): untuk menghubungkan mikrofon pada komputer.
+        `,
+        link    : ""
+    },
+    {
+        name    : "19.	Pin CPU Fan",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/cpu-fan-300x168.jpg",
+        artikel : "Pin CPU Fan berfungsi untuk  memberikan daya ke kipas processor agar dia dapat menyala sehingga bisa mendinginkan processor.",
+        link    : ""
+    },
+    {
+        name    : "20.	Pin Front Panel",
+        img     : "https://www.kiosbarcode.com/wp-content/uploads/2019/11/f_panel.jpg",
+        artikel : "Pin Front Panel ini berfungsi sebagai konektor dari casing ke motherboard sehingga kita bisa menyalakan komputer melalui casing.",
+        link    : ""
+    }
+];
+
+var temp = "";
+var card = document.getElementById('list-komponen');
+komponens.forEach(komponen => {
+    temp = `${temp}
+    <div>
+    <div class="card card-1">
+    <img class="card-img" src="${komponen.img}" alt="${komponen.name}">
+    <a href="${komponen.link}">
+    <h1>${komponen.name}</h1>
+    </a>
+    <p class="card-text">${komponen.artikel}</p>
+    </div>
+    </div>
+    `
+});
+
+card.innerHTML = temp;
